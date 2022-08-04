5ea6c317da56fbe8af8cf8c1a57b2f6ec1d81e93
monochromer <monochromer@mail.ru>
2022-08-04 10:07:49 +0500

    Обработка заголовоков, выписок и social-данных

--- src/services/getTransformedArticles.js
+++ src/services/getTransformedArticles.js
@@ -2,7 +2,7 @@ function getTransformedArticles(articles) {
   return articles
     .map(articleData => ({
       id: articleData.data.id,
-      title: articleData.data.title,
+      title: articleData.data.markdownData.title,
       link: articleData.data.link,
       createdAt: articleData.data.createdAt,
       updatedAt: articleData.data.updatedAt,
@@ -10,7 +10,7 @@ function getTransformedArticles(articles) {
         return articleData.data.populatedCategories
       },
       get content() {
-        return articleData.templateContent
+        return articleData.data.markdownData.content
       }
     }))
 }
